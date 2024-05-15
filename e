404bf4b79f7f0cb3cdd7348a45c3ffa98306eb13
dewa <rafifsyah1314@gmail.com>
2024-05-16 05:31:12 +0700

    [upadte tempalte]

---
+++ e
@@ -0,0 +1,35 @@
+[33mcommit 351b1ba4dbc9ba2165e7887a4999cc3692bc41cb[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: dewa <rafifsyah1314@gmail.com>
+Date:   Wed May 15 23:40:27 2024 +0700
+
+    [upadte tempalte]
+
+[33mcommit 3f77102fdf3db13f9c0c7868cdde5e9eb42ee069[m
+Author: dewa <rafifsyah1314@gmail.com>
+Date:   Wed May 15 23:12:39 2024 +0700
+
+    [upadte readme]
+
+[33mcommit 1e16337837353f075eabc74cefa78495f10babc0[m
+Author: dewa <rafifsyah1314@gmail.com>
+Date:   Wed May 15 23:10:43 2024 +0700
+
+    [upadte readme]
+
+[33mcommit 7c0b4153aa645b9e23f19aff600f32782c88b6da[m
+Author: dewa <rafifsyah1314@gmail.com>
+Date:   Wed May 15 23:09:05 2024 +0700
+
+    [upadte readme]
+
+[33mcommit 64a98bc31c6ea02f79702081dd414bc0995916cd[m
+Author: dewa <rafifsyah1314@gmail.com>
+Date:   Wed May 15 21:47:52 2024 +0700
+
+    [add] all
+
+[33mcommit a1ffcb1f9ae6f4f2ad4cefbadd6a38fb11194b1e[m
+Author: dewa <rafifsyah1314@gmail.com>
+Date:   Wed May 15 21:34:59 2024 +0700
+
+    perdana
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Orange &mdash; Free Website Template, Free HTML5 Template by FreeHTML5.co</title>
+	<title>Gallery ku &mdash; berbbagi cerita melalui foto</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
 	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
@@ -38,16 +38,16 @@
 	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
 	
 	<!-- Animate.css -->
-	<link rel="stylesheet" href="../css/animate.css">
+	<link rel="stylesheet" href="{{ asset("css/animate.css") }}">
 	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="../css/icomoon.css">
+	<link rel="stylesheet" href="{{ asset("css/icomoon.css") }}">
 	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="{{ asset("css/bootstrap.css") }}">
 	<!-- Theme style  -->
-	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="{{ asset("css/bootstrap.css") }}">
 
 	<!-- Modernizr JS -->
-	<script src="../js/modernizr-2.6.2.min.js"></script>
+	<script src="{{ asset("js/modernizr-2.6.2.min.js") }}"></script>
 	<!-- FOR IE9 below -->
 	<!--[if lt IE 9]>
 	<script src="js/respond.min.js"></script>
@@ -389,15 +389,15 @@
 	</div>
 	
 	<!-- jQuery -->
-	<script src="../js/jquery.min.js"></script>
+	<script src="{{ asset("js/jquery.min.js") }}"></script>
 	<!-- jQuery Easing -->
-	<script src="../js/jquery.easing.1.3.js"></script>
+	<script src="{{ asset("js/jquery.easing.1.3.js") }}"></script>
 	<!-- Bootstrap -->
-	<script src="../js/bootstrap.min.js"></script>
+	<script src="{{ asset("js/bootstrap.min.js") }}"></script>
 	<!-- Waypoints -->
-	<script src="../js/jquery.waypoints.min.js"></script>
+	<script src="{{ asset("js/jquery.waypoints.min.js") }}"></script>
 	<!-- Main -->
-	<script src="../js/main.js"></script>
+	<script src="{{ asset("js/main.js") }}"></script>
 
 	</body>
 </html>
